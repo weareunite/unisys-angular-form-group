20d6f4b1e210362b5c8618b72a812516aa18a580
jakub.bajzath <bajzath.jakub@gmail.com>
2019-01-21 14:03:18 +0100

    Fixed package errors, added dependencies

--- src/lib/unisys-angular-form-group.module.ts
+++ src/lib/unisys-angular-form-group.module.ts
@@ -1,12 +1,30 @@
-import { NgModule } from '@angular/core';
-import { UnisysAngularFormGroupComponent } from './unisys-angular-form-group.component';
+import {NgModule} from '@angular/core';
+import {UnisysAngularFormGroupComponent} from './unisys-angular-form-group.component';
 import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {HttpClient} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [UnisysAngularFormGroupComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
   ],
   exports: [UnisysAngularFormGroupComponent]
 })
-export class UnisysAngularFormGroupModule { }
+export class UnisysAngularFormGroupModule {
+}
