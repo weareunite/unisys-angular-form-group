8e3ffa8defb1e0e67db0c27332ae95a9ce07803d
jakub.bajzath <bajzath.jakub@gmail.com>
2019-01-29 12:32:03 +0100

    0.3.1

--- src/lib/unisys-angular-form-group.module.ts
+++ src/lib/unisys-angular-form-group.module.ts
@@ -7,7 +7,7 @@ import {HttpClient} from '@angular/common/http';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, './assets/i18n/home/', '.json');
 }
 
 @NgModule({
